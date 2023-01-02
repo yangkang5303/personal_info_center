838a528fb230f089151f4bcaf744852a78c67266
Maxwell ano <yangkang5303@gmail.com>
2023-01-02 17:54:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ CONSUMER_SECRET = key.api_key_secret
 BEARER_TOKEN = key.bearer_token
 MY_TWITTER_CREDS = os.path.expanduser('~/.my_app_credentials')
 if not os.path.exists(MY_TWITTER_CREDS):
-	oauth_dance("mytt_Bangkok", CONSUMER_KEY, CONSUMER_SECRET, MY_TWITTER_CREDS)
+	oauth_dance("your_dance_name", CONSUMER_KEY, CONSUMER_SECRET, MY_TWITTER_CREDS)
 oauth_token, oauth_secret = read_token_file(MY_TWITTER_CREDS)
 t = Twitter(auth=OAuth(oauth_token, oauth_secret, CONSUMER_KEY, CONSUMER_SECRET))
 
